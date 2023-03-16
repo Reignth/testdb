24b6df48e51e1dbb8383a6b34fb655c9de6fe932
Reinaldy Thando <88071683+Reignth@users.noreply.github.com>
2023-03-16 22:16:52 +0700

    Update deletePet.php

--- deletePet.php
+++ deletePet.php
@@ -1,15 +1,11 @@
  <?php
+    header("Access-Control-Allow-Origin: *");
     $db = mysqli_connect('127.0.0.1', 'root', '', 'dbuser');
     if (!$db) {
         die("Connection failed: " . mysqli_connect_error());
     }
 
     $id_pet = $_POST['id_pet'];
-
-
-    // $del  = "DELETE a.*, b.* FROM pet a
-    // INNER JOIN vaccinecert b ON a.id_pet = b.id_pet
-    // WHERE a.id_pet='$id_pet'";
 
     $delpet = "DELETE * FROM pet WHERE id_pet = '$id_pet'";
     $query = mysqli_query($db, $delpet);
